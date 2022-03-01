b4fc94b50175ca0ef7da668e7b5f7167e3fed22f
LaptevPavel <laptev.graphics@gmail.com>
2022-03-01 18:31:45 +0100

    wip

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,11 +6,7 @@ const ControlDot: React.FunctionComponent = (props) => {
   return (
     <section className={styles.wrap}>
       <SocialMedia />
-      <h3 className={styles.introduction}>
-        Hi! My name is <span>Pavel Laptev</span>. I'm a digital designer. Love
-        design, frontend, art and architecture. Make things work, create
-        plugins, make pet-projects and write articles.
-      </h3>
+      <h1 className={styles.introduction}>STOP THE WAR! STOP THE CRIME! 🇺🇦</h1>
     </section>
   );
 };
