eab3325a259f7edc4105e245a90e1a96b02207aa
LaptevPavel <laptev.graphics@gmail.com>
2022-03-05 01:34:19 +0100

    wip

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,9 @@ const ControlDot: React.FunctionComponent = (props) => {
   return (
     <section className={styles.wrap}>
       <SocialMedia />
-      <h1 className={styles.introduction}>STOP THE WAR! STOP THE CRIME! 🇺🇦</h1>
+      <h1 className={styles.introduction}>
+        War is Peace, Freedom is Slavery, Ignorance is Strength
+      </h1>
     </section>
   );
 };
