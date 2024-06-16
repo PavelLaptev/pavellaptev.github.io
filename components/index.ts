b4a417bcff5b013e91fd30ebfa86093e6cc49c76
Pavel Laptev <pawellaptew@gmail.com>
2024-06-17 01:32:52 +0200

    update dependencies

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,5 @@
 export { default as Wrapper } from "./Wrapper";
 export { default as SocialLinks } from "./SocialLinks";
-export { default as BerlinPin } from "./BerlinPin";
 export { default as ThemeSwitcher } from "./ThemeSwitcher";
 export { default as Title } from "./Title";
 export { default as Section } from "./Section";
